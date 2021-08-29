19dcf8e9d3f811e71061d890785017dd16d477de
Sarah Barnes <seb0321@live.unc.edu>
2021-08-29 16:46:35 -0400

    Finished exercise01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,7 +1,7 @@
 """Hype Machine"""
 
 name: str = input("What is your name? ")
-print(name + " you rock!") 
+print(name + "you rock!") 
 how_it_be: str = input(name + ", how is school?")
 print("Yay! You are going to crush Comp 110!.")
 
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -6,4 +6,5 @@ print(left_hand_side + " / " + right_hand_side + " is " + str(int(left_hand_side
 print(left_hand_side + " // " + right_hand_side + " is " + str(int(left_hand_side) // int(right_hand_side)))
 print(left_hand_side + " % " + right_hand_side + " is " + str(int(left_hand_side) % int(right_hand_side)))
 
+
 __author__ = "730358517"
