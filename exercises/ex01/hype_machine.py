07823222ccdceef92fb6b300ac19b3051944204a
Sarah Barnes <seb0321@live.unc.edu>
2021-08-29 16:49:31 -0400

    Finished Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,8 +1,8 @@
 """Hype Machine"""
 
 name: str = input("What is your name? ")
-print(name + "you rock!") 
+print(name + " you rock!") 
 how_it_be: str = input(name + ", how is school?")
-print("Yay! You are going to crush Comp 110!.")
+print("Yay! You are going to crush Comp 110!")
 
 __author__ = "730358517"
