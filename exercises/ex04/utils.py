fcc6406c5cbe10b0eb1443fe52108a74bb014d19
Sarah Barnes <seb0321@live.unc.edu>
2021-09-26 16:15:33 -0400

    Completed Exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,50 @@
 """List utility functions."""
 
-__author__ = "123456789"
+
+def all(intlist, number) -> bool:
+    i: int = 0 
+    while i < len(intlist):
+        if intlist[i] != number:
+            return False
+        i += 1
+    return True
 
 
-# TODO: Implement your functions here.
+def is_equal(numberlist, numbercatalog) -> bool:
+    i: int = 0 
+    while i < len(numberlist):
+        if numberlist[i] != numbercatalog:
+            return False
+        i += 1
+    return True
+
+
+def max(input: list[int]) -> int:
+    m = input[0]
+    i = 0
+    while i < len(input):
+        if input[i] < m:
+            m = input[i]
+            return m 
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    
+        
+
+def main():
+    print(all([1, 2, 3], 1))
+    print(all([1, 1, 1], 2))
+    print(all([1, 1, 1], 1))
+
+    print(is_equal([1, 0, 1], [1, 0, 1]))
+    print(is_equal([1, 1, 0], [1, 0, 1]))
+
+    print(max([1, 3, 2])
+    print(max([100, 99, 98])
+    print(max([])
+
+if __name__ == "__main__":
+    main()
+
+__author__ = "730358517"
+
