e7cb164fdd7ae73840c506a4b0792a91d225a160
Sarah Barnes <seb0321@live.unc.edu>
2021-10-10 23:40:18 -0400

    Exercise 6

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,41 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
 
-# Define your functions below
+def invert(input: dict[str, str]) -> dict[str, str]:
+    input = dict()
+    for a in input.keys():
+        for b in input.keys():
+            if input[a] == input[b] and a != b: 
+                raise KeyError("You have an error.")
+    for i in input.keys():
+        input[input[i]] = i
+        input.pop(i)
+    return input
+    
+    
+def favorite_color(color: dict[str, str]) -> str:
+    max = 0 
+    curr = 0
+    for a in color.keys():
+        for b in color.keys():
+            if color[a] == color[b] and a != b:
+                curr += 1      
+        if curr > max:
+            maxkey = a
+            max = curr
+        curr = 0
+    return maxkey
+    
+
+def count(input: list[str]) -> dict[str, int]:
+    counter = dict()
+    for a in input:
+        counter[a] = 0
+    for a in counter.keys():
+        for b in input:
+            if list(b) == a:
+                counter[a] = counter[a] + 1
+    return counter
+
+
+__author__ = "730358517"
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,63 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count 
+
+
+def invert() -> None:
+    """Test Invert Empty."""
+    xs: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
+    assert invert(xs) == KeyError
+
+
+def invert_1() -> None:
+    """Test Invert 1."""
+    xs: dict[str, str] = {'a': 'z', 'b' : 'y', 'c': 'x'}
+    assert invert(xs) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def invert_2() -> None:
+    """Test Invert 1."""
+    xs: dict[str, str] = {'apple': 'cat'}
+    assert invert(xs) == {'cat': 'apple'}
+
+
+def favorite_color_1() -> None:
+    """Test Count Empty."""
+    xs: dict[str, str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(xs) == 'blue'
+
+
+def favorite_color_2() -> None:
+    """Test Count 1."""
+    xs: dict[str, str] = {"Marc": "yellow", "Ezri": "yellow", "Kris": "blue"}
+    assert favorite_color(xs) == 'yellow'
+
+
+def favorite_color_3() -> None:
+    """Test Count 2."""
+    xs: dict[str, str] = {"Marc": "orange", "Ezri": "orange", "Kris": "blue"}
+    assert favorite_color(xs) == 'orange'
+
+
+def favorite_color_empty() -> None:
+    """Test Favorite Color Empty."""
+    xs: list[int] = [1, 2, 3]
+    assert favorite_color(xs, yay) == [1, 2, 3, 6, 7, 11]
+
+
+def favorite_color_1() -> None:
+    """Test Favorite Color 1."""
+    xs: list[int] = []
+    yay: list[int] = []
+    assert favorite_color(xs, yay) == []
+
+
+def favorite_color_2() -> None:
+    """Test Favorite Color 2."""
+    xs: list[int] = [4, 5, 6]
+    yay: list[int] = [23, 2, 4]
+    assert favorite_color(xs, yay) == [4, 5, 6, 23, 2, 4]
+
+
+__author__ = "730358517"
